68227d28d2bd24974d55b2c3631b845e914e4428
HarisRahim-SF <haris_rahim@berkeley.edu>
2025-04-10 18:17:57 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import Demo from './pages/Demo';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -19,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/news" element={<News />} />
+          <Route path="/demo" element={<Demo />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
