1d56983a592a7b1b496ab7cfa3335419414feaca
HarisRahim-SF <haris_rahim@berkeley.edu>
2025-04-16 20:44:52 -0700

    Delete import testimonials

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import Features from '@/components/Features';
-import Testimonials from '@/components/Testimonials';
 import CallToAction from '@/components/CallToAction';
 import Footer from '@/components/Footer';
 
